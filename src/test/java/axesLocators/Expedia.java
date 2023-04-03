e9665d2ff1d6c03879550aa70a6a72cafddf41a7
LENOVO <LENOVO@DESKTOP-HQL3KN4>
2023-04-03 21:57:27 +0530

    *************

--- src/test/java/axesLocators/Expedia.java
+++ src/test/java/axesLocators/Expedia.java
@@ -1,9 +1,22 @@
 package axesLocators;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Expedia {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws InterruptedException {
+		WebDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.expedia.co.in/");
+		Thread.sleep(2000);
+		driver.manage().window().maximize();
+		driver.findElement(By.xpath("")).click();
+		driver.findElement(By.xpath("")).click();
+		driver.findElement(By.xpath("")).click();
+		driver.findElement(By.xpath("")).click();
+		driver.findElement(By.xpath("")).click();
 
 	}
 
--- src/test/java/axesLocators/Sensex.java
+++ src/test/java/axesLocators/Sensex.java
@@ -12,9 +12,22 @@ public class Sensex {
 		driver.get("https://www.moneycontrol.com/stocksmarketsindia/");
 		Thread.sleep(2000);
 		driver.manage().window().maximize();
-		driver.findElement(By.xpath("//a[@href=\"/indian-indices/nifty-50-9.html\"]/parent::td/parent::tr/child::td[2]"));
-		//driver.findElement(By.xpath("//div[@class=\"header-logo-container\"]/../div[2]/div/div[@class=\"sc-iveFHk wNxAQ\""));
+		//doubt//price
+		driver.findElement(By.xpath("//a[@href=\"/indian-indices/nifty-50-9.html\"]/parent::td/parent::tr/child::td[2]")).click();
+	//change
+		driver.findElement(By.xpath("//a[contains(text(),\"Axis Bank\")]/parent::td/parent::tr/child::td[3]"));
+//current
+		driver.findElement(By.xpath("//a[contains(text(),\"ICICI Bank\")]/parent::td/parent::tr/child::td[4]")).click();
+		//market
+		driver.findElement(By.xpath("//ul[@class=\"clearfix main_nav\"]/child::li/child::a[@title=\"News\"]")).click();
+		driver.findElement(By.xpath("(//span[@class=\"robo_medium\"])[1]/parent::td/parent::tr/child::td[@class=\"tbl_greentxt\"]")).click();
 
+
+		//doubt stockMarket
+		driver.findElement(By.xpath("(//section[@class=\"otherstock\"])[1]/child::div/child::div/child::div[text()=\"Stock Action\"]")).click();
 	}
+	
+	
+	
 
 }
